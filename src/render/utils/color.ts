9c18de953b1255d66f13f92c7afd6002f8380060
李啸 <lixiao@cyberpeace.cn>
2021-06-11 17:56:04 +0800

    clear

---
+++ src/render/utils/color.ts
@@ -0,0 +1,75 @@
+/*
+ * @Author: Lixiao2
+ * @Date: 2021-06-11 10:05:23
+ * @LastEditors: Lixiao
+ * @LastEditTime: 2021-06-11 11:58:06
+ * @Desciption: Do not edit
+ * @Email: 932184220@qq.com
+ */
+
+function colourBlend(c1: string, c2: string, ratio: number): string {
+  ratio = Math.max(Math.min(Number(ratio), 1), 0)
+  let r1 = parseInt(c1.substring(1, 3), 16)
+  let g1 = parseInt(c1.substring(3, 5), 16)
+  let b1 = parseInt(c1.substring(5, 7), 16)
+  let r2 = parseInt(c2.substring(1, 3), 16)
+  let g2 = parseInt(c2.substring(3, 5), 16)
+  let b2 = parseInt(c2.substring(5, 7), 16)
+  let r = Math.round(r1 * (1 - ratio) + r2 * ratio) + ""
+  let g = Math.round(g1 * (1 - ratio) + g2 * ratio) + ""
+  let b = Math.round(b1 * (1 - ratio) + b2 * ratio) + ""
+  r = parseInt(r).toString(16)
+  g = parseInt(g).toString(16)
+  b = parseInt(b).toString(16)
+  r.length == 1 && (r = "0" + r)
+  g.length == 1 && (g = "0" + g)
+  b.length == 1 && (b = "0" + b)
+  return "#" + r + g + b
+}
+
+function LightenDarkenColor(col: string, amt: number): string {
+  let usePound = false
+  if (col[0] == "#") {
+    col = col.slice(1)
+    usePound = true
+  }
+  let num = parseInt(col, 16)
+  let r = (num >> 16) + amt
+  if (r > 255) r = 255
+  else if (r < 0) r = 0
+  let b = ((num >> 8) & 0x00ff) + amt
+  if (b > 255) b = 255
+  else if (b < 0) b = 0
+  let g = (num & 0x0000ff) + amt
+  if (g > 255) g = 255
+  else if (g < 0) g = 0
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16)
+}
+
+type colorType = {
+  [key: string]: string
+  "--theme-color": string
+  "--primary-color": string
+  "--primary-hover-color": string
+  "--font-dark-color": string
+  "--font-light-color": string
+  "--border-dark-color": string
+  "--border-light-color": string
+}
+
+export function createColor(color: string): string {
+  let theme: colorType = {
+    "--theme-color": color,
+    "--primary-color": color,
+    "--primary-hover-color": colourBlend(color, "#FFFFFF", 0.2),
+    "--font-dark-color": colourBlend(color, "#000000", 0.7),
+    "--font-light-color": colourBlend(color, "#FFFFFF", 0.9),
+    "--border-dark-color": LightenDarkenColor(colourBlend(color, "#000000", 0.9), 50),
+    "--border-light-color": LightenDarkenColor(colourBlend(color, "#FFFFFF", 0.9), -30),
+  }
+  let str: string = ""
+  for (const key in theme) {
+    str += `${key}: ${theme[key]};`
+  }
+  return str
+}
